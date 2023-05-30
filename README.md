259a4808e6d86739e21a2b0226c8303dc8074ada
Vinicius Fortunato <vinedfs@outlook.com>
2023-05-30 20:40:41 -0300

    Setup EditorConfig

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
  - React
  - React Router DOM
  - ESLint
+ - EditorConfig
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,38 +2,38 @@ import { useCallback } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleSubmit = useCallback((event) => {
-        event.preventDefault();
-        navigate('/');
-    }, [navigate]);
+	const handleSubmit = useCallback((event) => {
+		event.preventDefault();
+		navigate('/');
+	}, [navigate]);
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="input-email">E-mail</label>
-                    <input 
-                        type="email"
-                        id="input-email"
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="input-password">Senha</label>
-                    <input 
-                        type="password"
-                        id="input-password"
-                        required
-                    />
-                </div>
-                <button type="submit">Entrar</button>
-            </form>
-            <Link to="/">Voltar</Link>
-        </div>
-    );
+	return (
+		<div>
+			<h1>Login</h1>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="input-email">E-mail</label>
+					<input
+						type="email"
+						id="input-email"
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor="input-password">Senha</label>
+					<input
+						type="password"
+						id="input-password"
+						required
+					/>
+				</div>
+				<button type="submit">Entrar</button>
+			</form>
+			<Link to="/">Voltar</Link>
+		</div>
+	);
 }
 
 export default Login;
